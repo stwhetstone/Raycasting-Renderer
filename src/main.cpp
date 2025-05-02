d36b29fc730ea7f056b3b0e2170f8f7218f3434b
Stephen <stwhetstone@gmail.com>
2025-05-01 22:38:17 -0400

    DDA

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,63 @@
 #include "SDL3/SDL.h"
 #include "SDL3/SDL_main.h"
 
-#include "Vec2f.hpp"
+#include "Vec2.hpp"
 
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
 
+#define MAP_WIDTH 24
+#define MAP_HEIGHT 24
+
+const int worldMap[MAP_WIDTH][MAP_HEIGHT] = {
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+
+
+
 int main(int argc, char **argv) {
     if(!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
-		return -1;
-	}
+        return -1;
+    }
 
-	SDL_Window *window = SDL_CreateWindow("boids", 1280, 720, 0);
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
-	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_TARGET, WINDOW_WIDTH, WINDOW_HEIGHT);
+    SDL_Window *window = SDL_CreateWindow("Raycasting", 1280, 720, 0);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_TARGET, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-	if(!window || !renderer || !texture) {
-		return -1;
-	}
-	
-	SDL_SetRenderVSync(renderer, 1);
+    if(!window || !renderer || !texture) {
+        return -1;
+    }
 
-    Vec2f pos(WINDOW_WIDTH / 2.0, WINDOW_HEIGHT / 2.0);
+    SDL_SetRenderVSync(renderer, 1);
 
     SDL_Event e;
     bool quit = false;
+
+
+    Vec2<float> pos(22, 12), dir(-1, 0), cameraPlane(0, 0.66);
 
     while(!quit) {
         while(SDL_PollEvent(&e) != 0) {
@@ -37,18 +70,85 @@ int main(int argc, char **argv) {
 
         SDL_SetRenderTarget(renderer, texture);
 
+        for(int x = 0; x < WINDOW_WIDTH; x++) {
+            // current point on camera plane
+            // makes left side of screen -1, center 0, right side 1
+            const double cameraX = 2 * x / (double) WINDOW_WIDTH - 1;
+            Vec2<float> rayDir = dir + cameraPlane * cameraX;
+            
+            // what square of the map are we in
+            Vec2<int> mapPos((int)pos.x(), (int)pos.y());
+            // distance from original point in x/y direction to some side of a cell that the ray is currently intersection
+            Vec2<float> sideDist; 
+            // distance to move if you wanted to go 1 space in the x/y direction
+            // scale x/y so that when x/y = 1, a right triangle is made, then step size is that triangle's hypotenuse
+            //
+            // side dx = -1, side dy = -0.66
+            // when dx = 1 => dy = -0.66 / -1 = 0.66
+            // when dy = 1 => dx = -1 / -0.66 = 1.515
+            //
+            // rayUnitStepSize.x = sqrt( (dx/dx)^2 + (dy / dx)^2 ) = sqrt( 1 + (0.66)^2 )
+            // rayUnitStepSize.y = sqrt( (dy/dy)^2 + (dx / dy)^2 ) = sqrt( 1 + (1.515) ^2 )
+            Vec2<float> rayUnitStepSize(
+                                    rayDir.x() == 0 ? 1e30 : std::sqrt( 1 + (rayDir.y() * rayDir.y()) / (rayDir.x() * rayDir.x()) ),
+                                    rayDir.y() == 0 ? 1e30 : std::sqrt( 1 + (rayDir.x() * rayDir.x()) / (rayDir.y() * rayDir.y()) )
+                                );
+            // the direction that the ray moves in
+            Vec2<int> step;
+
+            bool hit = false;
+            int side = 0;
+
+            // distance between nearest x side in direction of rayDir and current point
+            if(rayDir.x() < 0) {
+                step.x(-1);
+                sideDist.x( (pos.x() - mapPos.x()) * rayUnitStepSize.x() );
+            } else if (rayDir.x() >= 0) {
+                step.x(1);
+                sideDist.x( (mapPos.x() + 1.0f - pos.x()) * rayUnitStepSize.x() );
+            }
+
+            // distance between nearest y side in direction of rayDir and current point
+            if(rayDir.y() < 0) {
+                step.y(-1);
+                sideDist.y( (pos.y() - mapPos.y()) * rayUnitStepSize.y() );
+            } else if (rayDir.y() >= 0) {
+                step.y(1);
+                sideDist.y( (mapPos.y() + 1.0f - pos.y()) * rayUnitStepSize.y() );
+            }
+
+            // DDA 
+            while(!hit) {
+                if(sideDist.x() < sideDist.y()) {
+                    // move the ray 1 unit in the x direction and rayDir.y / rayDir.x in the y direction
+                    sideDist.x( sideDist.x() + rayUnitStepSize.x() );
+                    // move mapPos to the next square in the x direction
+                    mapPos.x( mapPos.x() + step.x());
+                    side = 0;
+                } else if(sideDist.x() >= sideDist.y()) {
+                    // move the ray 1 unit in the y direction and rayDir.x / rayDir.y in the x direction
+                    sideDist.y( sideDist.y() + rayUnitStepSize.y() );
+                    // move mapPos to the next square in the y direction
+                    mapPos.y( mapPos.y() + step.y());
+                    side = 1;
+                }
+
+                if(worldMap[mapPos.x()][mapPos.y()] > 0) {
+                    hit = true;
+                }
+            }
+            
+        }
 
         
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_RenderClear(renderer);
-
 
 
         SDL_SetRenderTarget(renderer, nullptr);
-		SDL_RenderTexture(renderer, texture, nullptr, nullptr);
+        SDL_RenderTexture(renderer, texture, nullptr, nullptr);
 
         SDL_RenderPresent(renderer);
     }
+
     SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
